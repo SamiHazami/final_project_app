adcab6f311b3e2c9ff747fd6641353725f042b6f
SamiHazami <sa.hazemi@gmail.com>
2022-10-01 17:51:00 +0100

    update

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,7 +3,7 @@ import React from "react";
 const contacts = () => {
   return (
     <div>
-        <h1>Hello from contacts Page!</h1>
+        <h1></h1>
                 
     </div>
   );
--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const News = () => {
-  return <div>The weather like is nice today</div>;
+  return <div></div>;
 };
 
 export default News;
