981bfe102b3265b86ebf8f80b7d9adfe835e0967
SamiHazami <sa.hazemi@gmail.com>
2022-10-02 08:23:53 +0100

    update

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,10 +3,10 @@ import React from "react";
 const contacts = () => {
   return (
      <div>
-    <header style={{display:'flex',justifyContent:'space-between',alignItems:'center',padding:"1rem",margin:"0.5rem",borderRadius:"0.5rem",backgroundColor:'#e0e0e0'}} >
-        <h1>To contact us just fellow the roadmap</h1>
+    <header style={{ textalign:'jsutify',padding:"1rem",margin:"0.5rem",borderRadius:"0.5rem",backgroundColor:'#e0e0e0'}} >
+        <h1 style={{}}>To contact us just fellow the roadmap</h1>
     </header>
-    <section style={{padding:'10rem',margin:"0.5rem",borderRadius:"0.5rem",justicontent:'space-around',backgroundColor:'#e0e0e0'}}>
+    <section style={{padding:'10rem',margin:"0.5rem",borderRadius:"0.5rem",justifycontent:'space-around',backgroundColor:'#e0e0e0'}}>
       <img src='./assests/adresse.jpg' style={{width:"100%" }}/>
     </section>                
     </div>
